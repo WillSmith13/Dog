d7abcf01a5d7f1c8da58fe8fe20084ecea1ce177
wills <wills@localhost>
2016-03-02 12:59:32 -0500

    Dog Code put in

---
+++ DogWKS/src/edu/saintjoe/cs/wills/dogWKS/DogTestDrive.java
@@ -0,0 +1,13 @@
+package edu.saintjoe.cs.wills.dogWKS;
+
+public class DogTestDrive {
+
+	public static void main(String[] args) {
+		// Dog test code goes here 
+		Dog d = new Dog();
+		d.size = 40;
+		d.bark();
+
+	}
+
+}
